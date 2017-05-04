8b4fa0dad7a2ba3ff6ea4a22a4e5802e45135cc7
manojlovicsmilos <manojlovicsmilos@hotmail.com>
2017-05-04 17:15:59 +0200

    Tokio Tileset 2

--- PerfectPidgeonGameMechanic/Game.cs
+++ PerfectPidgeonGameMechanic/Game.cs
@@ -66,7 +66,7 @@ namespace PerfectPidgeonGameMechanic
             DForm.KeyPressed += new DrawForm.KeyPressedDelegate(this.KeyPressed);
             DForm.LeftRotate += new DrawForm.AxisRotate(this.LeftRotate);
             this._DataPool = new BaseDataPool();
-            StartLevel(this._DataPool.Levels["LVL01"]);
+            StartLevel(this._DataPool.Levels["Japan-Test"]);
             Time = new System.Timers.Timer(10);
             Time.Elapsed += new System.Timers.ElapsedEventHandler(TimerEvent_Tick);
             Time.Start();
